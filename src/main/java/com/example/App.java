d8b0ccd03a65265c4b9282ab983c9d403eb755be
Ilan H <takeishii18@hotmail.fr>
2023-04-16 01:21:13 +0200

    e

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int a = 1/2;
+        int b = a/5;
+    }
+}
